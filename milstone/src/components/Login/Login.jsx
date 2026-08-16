77c1bec3441627e4269196e00badf161718623ab
mohab457 <mohamedmohab199@gmail.com>
2026-08-16 17:00:41 +0300

    Login Form

--- milstone/src/components/Login/Login.jsx
+++ milstone/src/components/Login/Login.jsx
@@ -5,6 +5,58 @@ import Style from './Login.module.css'
 
 export default function Login() {
   return<>
-    <h1 className='text-center'>Login Component</h1>
+
+
+
+
+<div className="container d-flex align-items-center justify-content-center mb-3 mt-3">
+      <div className="row justify-content-center w-100">
+        <div className="col-12 col-md-6 col-lg-4">
+          <div className="card shadow-sm border-0 rounded-3 p-4 bg-light">
+            <h2 className="text-center mb-4 fw-bold text-dark">Login</h2>
+
+            <form>
+
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+
+                  placeholder="name@example.com"
+                  required
+                />
+              </div>
+
+              <div className="mb-4">
+                <label htmlFor="phone" className="form-label">
+                  password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="phone"
+                  name="phone"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary w-100 py-2">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+
+
+
   </>
 }
--- milstone/src/components/Navbar/Navbar.jsx
+++ milstone/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   return<>
 <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
   <div className="container-fluid">
-    <Link to={'/'} className="navbar-brand btn " href="#">Complaint System</Link>
+    <Link to={'/'} className="navbar-brand btn " href="#">Complaint System </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
