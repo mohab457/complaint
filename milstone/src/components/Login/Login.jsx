6e69b0c02a68496505b128558c5dd8e4cb8d91a7
mohab457 <mohamedmohab199@gmail.com>
2026-08-18 04:46:56 +0300

    useEffect issue handeld

--- milstone/src/components/Login/Login.jsx
+++ milstone/src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import React from 'react'
 import Style from './Login.module.css'
 import { Link } from 'react-router-dom'
